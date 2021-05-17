349081a96fc1f467492632e2c14980a1eed41a88
TomSanAndreas <tom.windels@outlook.com>
2021-05-17 18:01:06 +0200

    Button description fix

--- src/ui_base.cpp
+++ src/ui_base.cpp
@@ -1,9 +1,9 @@
 #include "ui_base.hpp"
 
 const GraphContext GraphContext::data[] = {
-    {"uA", "mA", "mV", "IC en VCE i.f.v. IB",  {"IC en VCE i.f.v. IB", "VCE i.f.v. IC", "VBE i.f.v. IC"}, 1.0, 1000.0, 1.0, true},
-    {"mV", "uA", "", "IC i.f.v. VCE", {"IC en VCE i.f.v. IB", "VCE i.f.v. IC", "VBE i.f.v. IC"}, 1.0, 1.0, 1.0, false},
-    {"mV", "mA", "", "IC i.f.v. VBE", {"IC en VCE i.f.v. IB", "VCE i.f.v. IC", "VBE i.f.v. IC"}, 1.0, 1000.0, 1.0, false}
+    {"uA", "mA", "mV", "IC en VCE i.f.v. IB",  {"IC en VCE i.f.v. IB", "IC i.f.v. VCE", "IC i.f.v. VBE"}, 1.0, 1000.0, 1.0, true},
+    {"mV", "uA", "", "IC i.f.v. VCE", {"IC en VCE i.f.v. IB", "IC i.f.v. VCE", "IC i.f.v. VBE"}, 1.0, 1.0, 1.0, false},
+    {"mV", "mA", "", "IC i.f.v. VBE", {"IC en VCE i.f.v. IB", "IC i.f.v. VCE", "IC i.f.v. VBE"}, 1.0, 1000.0, 1.0, false}
 };
 
 GraphType Graph::graphType;
